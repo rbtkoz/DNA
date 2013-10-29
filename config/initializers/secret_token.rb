f95037063d5947c6ba59b955fcc4e8c7aa7363c8
AK <kozovski.a@gmail.com>
2013-10-29 13:08:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DNA::Application.config.secret_key_base = '91fe6b285790845fa9f0ba55dbec48f74407b62e77a59c2efabb1d6454c74f8444e5b7e4f138172a06a30beb3e25283e2829543b410203baf62d5a0ee58c9b4c'
